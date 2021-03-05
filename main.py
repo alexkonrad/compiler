f02698e4eb7c0363d4d728e99bb338e41c8c628a
alex <alexkonrad08@gmail.com>
2021-03-05 09:57:05 -0800

    Main code for Register Machine

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from compiler.parser import Parser
 from compiler.symbol_table import SymbolTable
-
+from compiler.opcode_generator import OpCodeGenerator
+from dlx import DLX
 if __name__ == '__main__':
     # Parser.compile("1+2*(3+4)")
     SymbolTable.symbol_table["a"] = 3
     SymbolTable.symbol_table["b"] = 4
     Parser.compile("a+b*(3+4)")
+    buf = OpCodeGenerator.buf
+    DLX.load(buf)
+    DLX.execute()
+
